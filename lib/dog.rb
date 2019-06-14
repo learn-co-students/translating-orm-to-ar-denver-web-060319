1484fb9bf118acf68c89d69eed5a303cf3e40b7c
github username <github email address>
2019-06-14 10:25:17 -0600

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,10 @@
-class Dog
+class Dog < ActiveRecord::Base
+  # attr_accessor :name, :breed
+  # attr_reader :id
+
+  # def initialize(name:, breed:)
+  #   @name = name
+  #   @breed = breed
+  # end
+
 end
